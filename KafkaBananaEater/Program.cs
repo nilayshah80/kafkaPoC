b4f2eac84980e230b96c84cbc918f96268b6aa77
nilayshah80 <nilay.shah3nov@outlook.com>
2019-09-29 18:32:20 +0530

    Producer - Consumer .net

---
+++ KafkaBananaEater/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaBananaEater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "banana-consumers",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("banana-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true) 
+                    {
+                        try 
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            //Console.WriteLine(cr.Value);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch ( ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
